9f2679efb1aada9be90fc5c6842d85149fc9843e
umairkhanofficial <umairkhansadiq@gmail.com>
2024-04-01 17:00:00 +0500

    pytest

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from studentcrud.main import app
+
+def test_root_path():
+    client = TestClient(app=app)
+    response = client.get("/students")
+    assert response.status_code == 200
+    assert response.json() == [{"studentID":1,
+     "Name":"Umair",
+     "Age":26,
+     "Class": "Api"}]
+    
+def test_single_student():
+    client = TestClient(app=app)
+    response = client.get("/singlestudent/1")
+    assert response.status_code == 200
+    assert response.json() == {"studentID":1,
+     "Name":"Umair",
+     "Age":26,
+     "Class": "Api"}
